1932bfaa26da1792c5606cbfb14e2a0685c93e5d
sgtxjosue <sgtxjosue@gmail.com>
2018-08-22 22:53:15 -0600

    Login trasladdado listo

--- src/edu/ulatina/diariofacil/patrones/tester.java
+++ src/edu/ulatina/diariofacil/patrones/tester.java
@@ -1,5 +1,6 @@
 package edu.ulatina.diariofacil.patrones;
 import edu.ulatina.diariofacil.dao.UsuarioDAO;
+import edu.ulatina.diariofacil.model.DiarioFacil;
 import edu.ulatina.diariofacil.model.Usuario;
 import java.util.List;
 import java.util.Scanner;
@@ -7,20 +8,12 @@ import java.util.Scanner;
 
 
 public class tester {
+    
 
     public static void main(String[] args) {
-        List<Usuario> lstUsuarios=new UsuarioDAO().obtenerUsuarios();
-        Scanner sc=new Scanner(System.in);
-        String nombre,contrasena;
-        System.out.println("Ingrese su nombre:");
-        nombre=sc.nextLine();
-        System.out.println("Ingrese su contraseña:");
-        contrasena=sc.nextLine();
-        for (Usuario U : lstUsuarios) {
-            if(U.getNombre().equals(nombre) &&U.getContrasena().equals(contrasena)){
-                U.menuPrincipal();
-            }   
-        }
+        
+        DiarioFacil df=new DiarioFacil();
+        df.LogInDiarioFacil();
 
         
                 
